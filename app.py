c72c54ff9c8a4d466edf439cf2141fa429263496
Tadas Petra <tadas@tadaspetra.com>
2023-03-28 10:36:48 -0500

    feat: create endpoint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import json
 from flask_cors import CORS
 
-from utils import start_call, stop_call
+from utils import start_call, stop_call, start_transcription
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +10,8 @@ CORS(app)
 
 @app.route('/',)
 def endpoints():
-    app_routes = ['/start-call', '/stop-call/<sid>/<resource_id>']
+    app_routes = ['/start-call',
+                  '/stop-call/<sid>/<resource_id>', '/start-transcription']
     return json.dumps(app_routes)
 
 
@@ -26,6 +27,13 @@ def stop_recording(sid, resource_id):
     data = stop_call(resource_id, sid)
     context = {}
     return json.dumps(data)
+
+
+@app.route('/start-transcription', methods=['GET', 'POST'])
+def start_recording():
+    data = start_transcription()
+    context = data
+    return json.dumps(context)
 
 
 if __name__ == '__main__':
